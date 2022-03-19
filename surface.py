69e9fd9c8a46648272cc51d4650560d9b689c829
shaspen <shaspen@users.noreply.github.com>
2022-03-19 13:44:09 +0330

    commit

--- surface.py
+++ surface.py
@@ -1,7 +1,7 @@
 #This code written for solving https://quera.ir/problemset/33473/
 from cmath import pi
 
-def get_func(shape, args*):
+def get_func(shape, *args):
     if shape == 'square':
         return pow(args[0],2)
     elif shape == 'circle':
@@ -10,3 +10,11 @@ def get_func(shape, args*):
         return args[0]*args[1]
     elif shape == 'triangle':
         return args[0]*args[1]/2
+
+if __name__ == "__main__":
+    ls = get_func(['square', 'circle', 'rectangle', 'triangle'])
+
+    print(ls[0](1))
+    print(ls[1](2))
+    print(ls[2](2, 4))
+    print(ls[3](4, 5))
